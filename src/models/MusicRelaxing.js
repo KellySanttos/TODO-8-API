1f81e3bdc8078153c950b33c43e559a27b9547d9
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-08-22 00:04:10 -0300

    criei a pasta 'models' onde irei passar os modelos para o banco de dados.

---
+++ src/models/MusicRelaxing.js
@@ -0,0 +1,12 @@
+class Musicas {
+    constructor(nome, cantor, duracao, album){
+        this.id = 1;
+        this.nome = nome;
+        this.cantor = cantor;
+        this.duracao = duracao;
+        this.album = album;
+    }
+}
+
+
+export default Musicas;
